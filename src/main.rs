70f9dc4e4502b45e404a0d026b2806a6948748d7
SudhirYadav <sudhirkyadav51@gmail.com>
2024-12-23 03:01:26 +0000

    get request in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use error_chain::error_chain;
+use std::io::Read;
+
+error_chain! {
+    foreign_links{
+        Io(std::io::Error);
+        HttpRequest(reqwest::Error);
+    }
+}
+
+fn main() -> Result<()> {
     println!("Hello, world!");
+    let mut res = reqwest::blocking::get("http://httpbin.org/get")?;
+    let mut body = String::new();
+    res.read_to_string(&mut body)?;
+    println!("status:{:?}", res.status());
+    println!("body {:?}", body);
+    Ok({})
 }
